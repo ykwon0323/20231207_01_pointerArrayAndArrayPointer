b4b50ff6363ad532ba89ab09d6c97889078f8b08
ykwon0323 <ykwon0323@gmail.com>
2023-12-08 12:44:50 +0100

    study

--- pointerArray.c
+++ pointerArray.c
@@ -8,3 +8,16 @@
  * 즉, 포인터 변수를 저장할 수 있는 배열을 의미
  * 
 */
+# include <stdio.h>
+
+int main(void){
+    int i, arr_len;
+    int num01 = 10, num02 = 20, num03 = 30;
+    int* arr[3] = {&num01, &num02, &num03}; // int형 포인터 배열 선언
+
+    arr_len = sizeof(arr)/sizeof(arr[0]);
+    for(i = 0; i < arr_len; i++){
+        printf("%d\n", *arr[i]);
+    }
+
+}
